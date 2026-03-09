5a5664ffa356c1717a2e2a635e68bd4f626cb720
ekremyilmaz3416-maker <ekremyilmaz3416@gmail.com>
2026-03-09 14:42:11 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:41:56.090Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-09T11:42:12.038Z
+Commit: Fix race condition in async operations
