37d4c6f321e1dcfbb5c6a1914ef68fb5f5552ea5
ekremyilmaz3416-maker <ekremyilmaz3416@gmail.com>
2026-03-09 14:41:55 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:24:57.277Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-09T11:41:56.090Z
+Commit: Fix memory leak in event handlers
